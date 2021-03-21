ca2c2fb0051fa1ccab637a98d8572693e13ad259
foulow <jeffreyissaul@hotmail.com>
2021-03-21 09:00:11 -0400

    Factories added.

---
+++ Source/SimpleForex.API/Factories/CommandFactory.cs
@@ -0,0 +1,29 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using SimpleForex.Core.Contracts;
+
+namespace SimpleForex.API.Factories
+{
+    /// <summary>
+    /// Create an instance of an command.
+    /// </summary>
+    public class CommandFactory : ICommandFactory
+    {
+        protected readonly IServiceProvider _serviceProvider;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="serviceProvider">DI container.</param>
+        public CommandFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        /// <inheritdoc/>
+        public TCommand MakeCommand<TCommand>() where TCommand : ICommandBase
+        {
+            return _serviceProvider.GetRequiredService<TCommand>();
+        }
+    }
+}
